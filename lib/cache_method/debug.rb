475909a8e5326587cf2a1156f47709f27a2183c4
Seamus Abshere <seamus@abshere.net>
2012-04-24 17:01:45 -0500

    better debug logging

--- lib/cache_method/debug.rb
+++ lib/cache_method/debug.rb
@@ -1,8 +1,34 @@
 module CacheMethod
   def CacheMethod.digest(obj)
     if (l = ::Marshal.dump(resolve_cache_key(obj)).length) > 500
-      $stderr.puts "[cache_method] Warn: #{l} long digest. #{obj.class} -> #{resolve_cache_key(obj).inspect}"
+      $stderr.puts "[cache_method] DIGEST (long?): #{resolve_cache_key(obj).inspect}"
     end
     ::Digest::SHA1.hexdigest ::Marshal.dump(resolve_cache_key(obj))
+  end
+
+  class CachedResult
+    def debug_get_wrapped
+      retval = original_get_wrapped
+      if retval
+        # $stderr.puts "[cache_method] GET: #{method_signature}(#{args.inspect})"
+      else
+        $stderr.puts "[cache_method] GET (miss!): #{method_signature}(#{args.inspect})"
+      end
+      retval
+    end
+    alias :original_get_wrapped :get_wrapped
+    alias :get_wrapped :debug_get_wrapped
+
+    def debug_set_wrapped
+      retval = original_set_wrapped
+      if (l = ::Marshal.dump(retval).length > 1000)
+        $stderr.puts "[cache_method] SET (long?): #{method_signature}(#{args.inspect}) -> #{retval.inspect}"
+      else
+        # $stderr.puts "[cache_method] SET: #{method_signature}(#{args.inspect})"
+      end
+      retval
+    end
+    alias :original_set_wrapped :set_wrapped
+    alias :set_wrapped :debug_set_wrapped
   end
 end
